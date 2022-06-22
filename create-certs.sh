7406c603ed6f7a9a759319350f7ec1dc4017350c
JackMorganNZ <jackmorgannz@gmail.com>
2022-06-22 12:25:42 +1200

    Reformat cert script

--- create-certs.sh
+++ create-certs.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 mkcert -install
 mkdir certs
-mkcert -key-file certs/key.pem -cert-file certs/cert.pem proxy.localhost email.localhost cs-unplugged.localhost *.cs-unplugged.localhost cs-field-guide.localhost *.cs-field-guide.localhost codewof.localhost *.codewof.localhost dthm4kaiako.localhost *.dthm4kaiako.localhost
+mkcert \
+    -key-file certs/key.pem \
+    -cert-file certs/cert.pem \
+    proxy.localhost \
+    email.localhost \
+    cs-unplugged.localhost \
+    *.cs-unplugged.localhost \
+    cs-field-guide.localhost \
+    *.cs-field-guide.localhost \
+    codewof.localhost \
+    *.codewof.localhost \
+    dthm4kaiako.localhost \
+    *.dthm4kaiako.localhost
